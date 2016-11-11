5fed6239bb75106578b2b872a16627032ae56267
Aimee Borda <bordaa@scss.tcd.ie>
2016-11-11 23:53:19 +0000

    anagram solver template

---
+++ src/AnagramSolver/AnagramSolver.java
@@ -0,0 +1,9 @@
+package AnagramSolver;
+
+import java.util.HashSet;
+
+public class AnagramSolver {
+    public String solveAnagram(String word, HashSet<String> dictionary) {
+        return "";
+    }
+}
---
+++ src/AnagramSolver/AnagramSolverTest.java
@@ -0,0 +1,36 @@
+package AnagramSolver;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class AnagramSolverTest {
+
+    AnagramSolver a;
+
+    @Before
+    public void setUp(){
+        a = new AnagramSolver();
+    }
+
+    @Test
+    public void testEmpty(){
+
+        assertEquals("should return empty string","",a.solveAnagram("word",null));
+        assertEquals("should return empty string","",a.solveAnagram("",null));
+        assertEquals("should return empty string","",a.solveAnagram(null,null));
+        assertEquals("should return empty string","",a.solveAnagram(null,new HashSet<String>()));
+    }
+
+    @Test
+    public void testAnagram(){
+
+        HashSet<String> dictionary = new HashSet<String>(){{add("hello"); add("world"); add("to"); add("the"); add("is"); add("we"); add("tooth"); }};
+        assertEquals("should solve anagram", "hello to the world",a.solveAnagram("elhloothtedrowl",dictionary));
+
+    }
+}
