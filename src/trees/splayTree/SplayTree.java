f48711b2a8e4fe11a4585b003cc3e8ee9f86b5e8
Aimee Borda <bordaa@scss.tcd.ie>
2016-12-09 07:25:05 +0000

    splay tree conintued

--- src/trees/splayTree/SplayTree.java
+++ src/trees/splayTree/SplayTree.java
@@ -20,20 +20,36 @@ public class SplayTree<Key extends Comparable<Key>,Val> {
         root = new TreeNode(k, v, nodes[0],nodes[1]);
     }
 
-    private TreeNode splay(Key k,  TreeNode gp, TreeNode parent, TreeNode node){
-        if(node !=null){
-            if(node.key.compareTo(k) < 0) {
-                gp = splay(k, parent,node, node.right);
-                return splayOperation(gp, node, node.right);
-            }else if(node.key.compareTo(k) > 0) {
-                gp = splay(k, parent,node, node.left);
-                return splayOperation(gp, node, node.left);
-            } else {
-                return splayOperation(gp, parent, node);
-            }
-        }
+    private TreeNode splay(Key k, TreeNode gp, TreeNode parent, TreeNode node) {
+        if (node != null) {
 
-        return null;
+            if (node.key.compareTo(k) < 0) {
+                node.right = splay(k, parent, node, node.right);
+                return splayOperation(gp,parent,node);
+            } else if (node.key.compareTo(k) > 0) {
+                node.left = splay(k, parent, node, node.left);
+                return splayOperation(gp,parent,node);
+            }
+
+        }
+        return gp;
+
+    }
+
+
+
+    private TreeNode rotateRight(TreeNode node) {
+        TreeNode x = node.left;
+        node.left = x.right;
+        x.right = node;
+        return x;
+    }
+
+    private TreeNode rotateLeft(TreeNode node) {
+        TreeNode x = node.right;
+        node.right = x.left;
+        x.left = node;
+        return x;
     }
 
     private TreeNode splayOperation(TreeNode gp, TreeNode parent, TreeNode node) {
@@ -43,63 +59,99 @@ public class SplayTree<Key extends Comparable<Key>,Val> {
         else if(parent.left.key.compareTo(node.key) == 0){
             //3 cases
             if(gp == null){
+                TreeNode temp = node.right;
+                node.right = parent;
+                parent.left = temp;
 
+                return node;
             }else if(gp.left.key.compareTo(parent.key) == 0){
+                TreeNode temp = node.right;
+                node.right = parent;
+                parent.left = temp;
 
+                temp = parent.right;
+                parent.right = gp;
+                gp.left = temp;
             }else { //if(gp.right.key.compareTo(parent.key) == 0)
+                TreeNode temp1 = node.left;
+                node.left = parent;
+                parent.right = temp1;
 
+                temp1 = node.right;
+                node.right = gp;
+                gp.left = temp1;
             }
         }else { //if(parent.right.key.compareTo(node.key) == 0)
             //3 cases
             if(gp == null){
+                TreeNode temp = node.left;
+                node.left = parent;
+                parent.right = temp;
 
+                return node;
             }else if(gp.right.key.compareTo(parent.key) == 0){
-
+                TreeNode temp = node.left;
+                node.left = parent;
+                parent.right = temp;
+                temp = parent.left;
+                parent.left = gp;
+                gp.right = temp;
             }else { //if(gp.left.key.compareTo(parent.key) == 0)
+                TreeNode temp1 = node.right;
+                node.right = parent;
+                parent.left = temp1;
+
+                temp1 = node.left;
+                node.left = gp;
+                gp.right = temp1;
             }
         }
 
-        return null;
+        return node;
     }
 
     public void splay(Key k){
-        root = splay(k,null, null, root);
+        root = splay(k, null, null, root);
     }
 
-    private TreeNode find(Key k) {
+    public Val find(Key k) {
 
         splay(k);
         if(!isEmpty() && root.key == k)
-            return root;
+            return (Val) root.value;
         else
             return null;
     }
 
    public void delete(Key k){
         splay(k);
-        if(!isEmpty(root)) {
-            root = join(root.left, root.right);
+
+       if(!isEmpty(root)) {
+           root = join(root.left, root.right);
         }
     }
 
     private TreeNode join(TreeNode left, TreeNode right) {
-        left = splay(max(left).key,null,null,left);
+        left = splay((Key) max(left).key, null,null,left);
         if(!isEmpty())
             left.right = right;
         return left;
     }
 
-    private TreeNode<Key,Val> max(TreeNode node){
-        while(node.right != null){
+    private TreeNode max(TreeNode node){
+        while(node != null && node.right != null){
             node = node.right;
         }
 
         return node;
     }
 
-    private TreeNode[] split(Key k){
+    public TreeNode[] split(Key k){
         splay(k);
-        return new TreeNode[]{root.left, root.right};
+        if(!isEmpty()) {
+            return new TreeNode[]{new TreeNode(root.key, root.value, root.left, null), root.right};
+        } else
+            return new TreeNode[]{null,null};
     }
 
     public Key getRoot(){
@@ -108,12 +160,12 @@ public class SplayTree<Key extends Comparable<Key>,Val> {
 
     @Override
     public String toString(){
-        return toString(root);
+        return toString(root).trim();
     }
 
     private String toString(TreeNode node) {
         if(node != null)
-            return toString(node.left) +" (" +node.key+", "+node.value +") "+toString(node.right);
+            return toString(node.left) +"(" +node.key+", "+node.value +") "+toString(node.right);
         else
             return "";
     }
--- src/trees/splayTree/SplayTreeTest.java
+++ src/trees/splayTree/SplayTreeTest.java
@@ -2,6 +2,7 @@ package trees.splayTree;
 
 
 import org.junit.Test;
+import trees.TreeNode;
 
 import static org.junit.Assert.*;
 
@@ -47,6 +48,37 @@ public class SplayTreeTest {
     }
 
     @Test
+    public void testSplit(){
+        SplayTree<Integer, Integer> tree = new SplayTree<>();
+        tree.insert(5,5);
+        tree.insert(4,4);
+        tree.insert(7,7);
+        tree.insert(9,9);
+
+        TreeNode[]nodes= tree.split(6);
+        assertNotNull(nodes[0]);
+        assertEquals("(5,5) (6,6)", nodes[0]);
+        assertEquals("(7,7) (9,9)", nodes[0]);
+        assertEquals("nodes 0 should not be null",4, nodes[0].key);
+        assertEquals("nodes 0 should not be null",9, nodes[1].key);
+    }
+
+    @Test
+    public void testFind(){
+        SplayTree<Integer, Integer> tree = new SplayTree<>();
+        tree.insert(5,5);
+        tree.insert(6,6);
+        tree.insert(7,7);
+
+        assertEquals((Integer)6, tree.find(6));
+        assertEquals((Integer)6, tree.getRoot());
+
+        assertEquals((Integer)7, tree.find(7));
+        assertEquals((Integer)7, tree.getRoot());
+
+        assertNull(tree.find(8));
+    }
+    @Test
     public void testString(){
         SplayTree<Integer, Integer> tree = new SplayTree<>();
         tree.insert(5,5);
@@ -57,9 +89,8 @@ public class SplayTreeTest {
 
         tree.delete(6);
         assertEquals("testing printing", "(5, 5) (7, 7)", tree.toString());
+
+        tree.delete(8);
+        assertEquals("testing printing", "(5, 5) (7, 7)", tree.toString());
     }
-
-
-
-
 }
